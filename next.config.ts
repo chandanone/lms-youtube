054d37c0390b3a56201b9517a54fef8cb4a246f0
chandanone <chandan868@gmail.com>
2026-01-15 12:36:00 +0530

    fix(next-config): allow ui-avatars.com for user profile images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   serverExternalPackages: ["sequelize", "pg"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/vi/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        pathname: "/api/**", // UI Avatars uses the /api path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
